2d76efbbf8b99d2d6fb329012ee823bc9b5f49dc
atia009 <aarontia09@gmail.com>
2022-12-15 11:05:37 -0800

    Refactor to use helper-function module

--- src/refresh.js
+++ src/refresh.js
@@ -1,18 +1,15 @@
+import appendElementToDOM from "./helper-functions";
 import { setImg } from "./image";
 
 const Refresh = () => {
   const btn = document.createElement("button");
   btn.textContent = "Refresh";
 
+  // cache DOM
+  appendElementToDOM(btn);
+
   // bind events
   btn.addEventListener("click", setImg);
-
-  // functions
-  function appendBtnToDOM(element) {
-    document.body.append(element);
-  }
-
-  appendBtnToDOM(btn);
 };
 
 export default Refresh;
